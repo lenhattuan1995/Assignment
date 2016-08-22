de9c7f766bad8990e0f9b34073f3332440a1c034
lenhattuan1995 <lenhattuan1995@gmail.com>
2016-08-22 10:44:02 +0700

    Up ứng dụng

---
+++ tuanlnps02484/DangNhap.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DangNhap
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnThoat = New System.Windows.Forms.Button()
+        Me.btnNhap = New System.Windows.Forms.Button()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnThoat
+        '
+        Me.btnThoat.BackColor = System.Drawing.Color.GhostWhite
+        Me.btnThoat.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnThoat.Location = New System.Drawing.Point(184, 98)
+        Me.btnThoat.Name = "btnThoat"
+        Me.btnThoat.Size = New System.Drawing.Size(88, 25)
+        Me.btnThoat.TabIndex = 11
+        Me.btnThoat.Text = "Thoát"
+        Me.btnThoat.UseVisualStyleBackColor = False
+        '
+        'btnNhap
+        '
+        Me.btnNhap.BackColor = System.Drawing.Color.GhostWhite
+        Me.btnNhap.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNhap.Location = New System.Drawing.Point(68, 98)
+        Me.btnNhap.Name = "btnNhap"
+        Me.btnNhap.Size = New System.Drawing.Size(89, 25)
+        Me.btnNhap.TabIndex = 10
+        Me.btnNhap.Text = "Đăng nhập"
+        Me.btnNhap.UseVisualStyleBackColor = False
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(110, 60)
+        Me.txt2.Name = "txt2"
+        Me.txt2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt2.Size = New System.Drawing.Size(182, 20)
+        Me.txt2.TabIndex = 9
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(110, 27)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(182, 20)
+        Me.txt1.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(36, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 16)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Mật khẩu"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(36, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Tài khoản"
+        '
+        'DangNhap
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.GhostWhite
+        Me.ClientSize = New System.Drawing.Size(340, 150)
+        Me.Controls.Add(Me.btnThoat)
+        Me.Controls.Add(Me.btnNhap)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "DangNhap"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnThoat As System.Windows.Forms.Button
+    Friend WithEvents btnNhap As System.Windows.Forms.Button
+    Friend WithEvents txt2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ tuanlnps02484/DangNhap.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Data.SqlClient
+Public Class DangNhap
+
+    Private Sub cmdnhap_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnNhap.Click
+        Dim chuoiketnoi As String = "workstation id=Ps02484.mssql.somee.com;packet size=4096;user id=tuanlnps02484_SQLLogin_2;pwd=1pf9nf5cos;data source=Ps02484.mssql.somee.com;persist security info=False;initial catalog=Ps02484"
+        Dim ketnoi As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim sqlAdater As New SqlDataAdapter("select * from NhanVien where TaiKhoan='" & txt1.Text & "' and MatKhau='" & txt2.Text & "'", ketnoi)
+        Dim tb As New DataTable
+
+        Try
+            ketnoi.Open()
+            sqlAdater.Fill(tb)
+            If tb.Rows.Count > 0 Then
+                MessageBox.Show("Đăng nhập thành công")
+                Main.Show()
+                Me.Hide()
+            Else
+                MessageBox.Show("Tài khoản hoặc mật khẩu không hợp lệ")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnthoat_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnThoat.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+End Class
+
